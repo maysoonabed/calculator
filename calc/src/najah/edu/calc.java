1a11601d7670e33906555c30b6f1c5e70855540c
Maysoon <Maysoon@DESKTOP-UMIFF6A>
2020-03-02 19:41:05 +0200

    tests

--- calc/src/najah/edu/calc.java
+++ calc/src/najah/edu/calc.java
@@ -1,7 +1,7 @@
 package najah.edu;
 
 public class calc {
-  public int add(int x, int y) {
+  public static int add(int x, int y) {
 	  return x + y;
   }
 }
---
+++ calc/src/najah/edu/calcTest.java
@@ -0,0 +1,20 @@
+package najah.edu;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class calcTest {
+
+	@Test
+	public void testAdd() {
+
+int pp =calc.add(3,4);
+assertTrue(pp == 7);
+int pn =calc.add(-3,4);
+assertTrue(pn == 1);
+int nn =calc.add(-3,-4);
+assertTrue(nn == -7);
+	}
+
+}
